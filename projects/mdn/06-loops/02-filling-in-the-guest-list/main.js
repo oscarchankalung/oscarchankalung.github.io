a0f47ebf7a1f0fcd08fdbb43bef7bdae28c14b09
Oscar Chan <oscarchankalung@gmail.com>
2020-10-27 14:57:51 +0800

    add loops: filling in the guest list

---
+++ projects/mdn/06-loops/02-filling-in-the-guest-list/main.js
@@ -0,0 +1,19 @@
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+    
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: '
+
+
+for (let i = 0; i < people.length; i++) {
+  if (people[i] === 'Phil' || people[i] === 'Lola') {
+    refused.textContent += people[i] + ', '
+  } else {
+    admitted.textContent += people[i] + ', '
+  }
+}
+
+refused.textContent = refused.textContent.replace(/(, $)/, '.');
+admitted.textContent = admitted.textContent.replace(/(, $)/, '.');
