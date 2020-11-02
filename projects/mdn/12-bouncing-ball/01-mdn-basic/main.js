372bd26f95bc87a7d9467dd99cdfe60673de0974
Oscar Chan <oscarchankalung@gmail.com>
2020-11-02 16:45:56 +0800

    add bouncing ball basic

---
+++ projects/mdn/12-bouncing-ball/01-mdn-basic/main.js
@@ -0,0 +1,48 @@
+// setup canvas
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = document.body.clientWidth;
+const height = canvas.height = document.body.clientHeight;
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomSign() {
+  return Math.random() < 0.5 ? -1 : 1;
+}
+
+let balls = [];
+
+while (balls.length < 25) {
+  let size = random(10,20);
+  let ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(2,7) * randomSign(),
+    random(2,7) * randomSign(),
+    `rgb(${random(0,255)}, ${random(0,255)}, ${random(0,255)})`,
+    size
+  );
+
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
