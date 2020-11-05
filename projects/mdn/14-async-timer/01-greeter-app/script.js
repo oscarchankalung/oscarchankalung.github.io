add5c34096ad57c4767899fd57b9087809444bbc
Oscar Chan <oscarchankalung@gmail.com>
2020-11-05 18:58:38 +0800

    minor bug fix (button disable)

--- projects/mdn/14-async-timer/01-greeter-app/script.js
+++ projects/mdn/14-async-timer/01-greeter-app/script.js
@@ -3,20 +3,24 @@ const greetBtn = document.querySelector('.greet');
 const cancelBtn = document.querySelector('.cancel');
 let greeting;
 
+input.value = "Oscar";
 cancelBtn.disabled = true;
 
 greetBtn.addEventListener('click', () => {
+  greetBtn.disabled = true;
   cancelBtn.disabled = false;
   greeting = setTimeout(greet, 5000, input.value);
 })
 
 cancelBtn.addEventListener('click', () => {
   clearTimeout(greeting);
+  greetBtn.disabled = false;
   cancelBtn.disabled = true;
   alert('Greeting cancalled!');
 })
 
 function greet(name) {
   alert(`Hello ${name}!`);
+  greetBtn.disabled = false;
   cancelBtn.disabled = true;
 }
