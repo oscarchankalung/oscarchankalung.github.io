272335fcb8dc6accb514a422e3d67b65a23bcd27
Oscar Chan <oscarchankalung@gmail.com>
2020-11-09 14:47:42 +0800

    add 08 simple fetch chain

---
+++ projects/mdn/14-async-timer/08-simple-fetch-chain/script.js
@@ -0,0 +1,17 @@
+fetch('../coffee.jpg')
+.then(response => {
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  } else {
+    return response.blob();
+  }
+})
+.then(myBlob => {
+  let objectURL = URL.createObjectURL(myBlob);
+  let image = document.createElement('img');
+  image.src = objectURL;
+  document.body.appendChild(image);
+})
+.catch(e => {
+  console.log(`There has been a problem with your fetch operation: ${e.message}`);
+})
