c147cc021baec242d629926d9f8d4c599c333fdf
Oscar Chan <oscarchankalung@gmail.com>
2020-11-09 14:37:05 +0800

    add 04 spinner

---
+++ projects/mdn/14-async-timer/04-spinner/script.js
@@ -0,0 +1,29 @@
+const spinner = document.querySelector('div');
+
+let spinning = false;
+let rotateCount = 0;
+let startTime = null;
+let rAF;
+
+document.body.addEventListener('click', () => {
+  spinning = !spinning;
+  spinning ? draw() : cancelAnimationFrame(rAF);
+})
+
+function draw(timestamp) {
+
+  // set startTime
+  if (!startTime) {
+    startTime = timestamp;
+  }
+
+  // set rotateCount
+  rotateCount = (timestamp - startTime) / 5;
+  
+  // set maximum rotateCount
+  if (rotateCount > 359) {
+    rotateCount %= 360;
+  }
+  spinner.style.transform = `rotate(${rotateCount}deg)`;
+  rAF = requestAnimationFrame(draw);
+}
