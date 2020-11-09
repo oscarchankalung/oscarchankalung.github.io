b89c42111946c1f941cb83f5bbe426258f698e8d
Oscar Chan <oscarchankalung@gmail.com>
2020-11-09 18:11:39 +0800

    add 01 simple fetch await

---
+++ projects/mdn/16-async-await/01-simple-fetch-await/script.js
@@ -0,0 +1,20 @@
+async function myFetch(path) {
+
+  let response = await fetch(path);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  } else {
+    let blob = await response.blob();
+    let objectURL = URL.createObjectURL(blob);
+    let image = document.createElement('img');
+    image.src = objectURL;
+    document.body.appendChild(image);
+  }
+  
+}
+
+myFetch('../coffee.jpg')
+.catch(e) {
+  console.log(e)
+}
