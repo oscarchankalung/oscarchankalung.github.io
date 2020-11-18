eacf24eeb39e30718edd7f5ba2146f0f8f2da1c8
Oscar Chan <oscarchankalung@gmail.com>
2020-11-18 18:41:36 +0800

    rename folder and add buttons

---
+++ projects/mdn/16-async-await/03-async-await-slow-fast/script.js
@@ -0,0 +1,48 @@
+function timeout(interval) {
+  return new Promise(resolve => setTimeout(resolve, interval));
+}
+
+async function slowTimeTest() {
+  await timeout(1000);
+  await timeout(1000);
+  await timeout(1000);
+}
+
+async function fastTimeTest() {
+  const timeout1 = timeout(1000);
+  const timeout2 = timeout(1000);
+  const timeout3 = timeout(1000);
+
+  await timeout1;
+  await timeout2;
+  await timeout3;
+}
+
+async function test(timeTest) {
+  let startTime = Date.now();
+  await timeTest();
+  let finishTime = Date.now();
+  let timeTaken = finishTime - startTime;
+  alert(`Time taken for ${timeTest.name}: ${timeTaken} milliseconds`);
+}
+
+async function main_async_await() {
+  await test(slowTimeTest);
+  await test(fastTimeTest);
+}
+
+function main_async_callback() {
+  test(slowTimeTest).then(() => test(fastTimeTest));
+}
+
+function main_sync_callback() {
+  test(slowTimeTest).then(test(fastTimeTest));
+}
+
+const btn1 = document.querySelector('#main-async-await');
+const btn2 = document.querySelector('#main-async-callback');
+const btn3 = document.querySelector('#main-sync-callback');
+
+btn1.addEventListener('click', main_async_await);
+btn2.addEventListener('click', main_async_callback);
+btn3.addEventListener('click', main_sync_callback);
